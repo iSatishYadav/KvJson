7df0512367a98fbacdeee5c3d54249662b5904f4
Satish Yadav <monstersatish@live.com>
2020-10-01 00:07:04 +0530

    Init. KV JSON Serializer added.

---
+++ KvJson.Tests/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace KvJson.Tests
+{
+    public class Person : IEquatable<Person>
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("website")]
+        public string Website { get; set; }
+
+        //public override string ToString() => string.Join("|", typeof(Person).GetProperties().Select(x=> $"{x.Name}:{x.GetValue(this)}"));
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&  Equals(person);
+        }
+
+        public bool Equals(Person other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   Name == other.Name &&
+                   Username == other.Username &&
+                   Email == other.Email &&
+                   Phone == other.Phone &&
+                   Website == other.Website;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Username, Email, Phone, Website);
+        }
+
+        public static bool operator ==(Person left, Person right)
+        {
+            return EqualityComparer<Person>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Person left, Person right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ KvJson/Serializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace KvJson
+{
+    public class Serializer<T> where T : new()
+    {
+        public string Serialize(IEnumerable<T> input)
+        {
+            //Get all properties and create a new JSON object with these properties names as keys and
+            //array of values of respective property in each element as values
+            //e.g. [{"id": 1, "name": "Satish" }, {"id": 2, "name": "Yadav"}]
+            //to: {"id": [1, 2], "name": ["Satish", "Yadav"]}
+            var props = typeof(T).GetProperties();
+            var headerAndValues = new Dictionary<string, List<dynamic>>();
+            foreach (var prop in props)
+            {
+                headerAndValues.Add(prop.Name, new List<dynamic>());
+            }
+            foreach (var item in input)
+            {
+                foreach (var prop in props)
+                {
+                    headerAndValues[prop.Name].Add(prop.GetValue(item));
+                }
+            }
+            return JsonSerializer.Serialize(headerAndValues);
+        }
+        public IEnumerable<T> Deserialize(string input)
+        {
+            var obj = JsonSerializer.Deserialize<JsonElement>(input);
+            var returnEnumberable = new List<T>();
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                int i = 0;
+                foreach (var item in obj.GetProperty(prop.Name).EnumerateArray())
+                {
+                    if (returnEnumberable.Count <= i)
+                        returnEnumberable.Add(new T());
+                    prop.SetValue(returnEnumberable[i], Convert.ChangeType(item.ToString(), prop.PropertyType));
+                    i++;
+                }
+            }
+            return returnEnumberable;
+            //throw new NotImplementedException();
+        }
+    }
+}
